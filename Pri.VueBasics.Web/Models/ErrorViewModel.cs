d8da3637beffccf6458a6710c5fe448e00cfe3e6
Mileto Di Marco <mileto.di.marco@howest.be>
2024-04-29 20:59:41 +0200

    Add project files.

---
+++ Pri.VueBasics.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pri.VueBasics.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Pri.VueBasics.Web/wwwroot/js/app.js
@@ -0,0 +1,24 @@
+﻿let vue = new Vue({
+    el: "#app",
+    data: {
+        pageTitle: "Our first Vue app",
+        url: "http://www.hln.be",
+        isVisible: false,
+        colors: ["Blue", "Yellow", "Green"],
+        quote: "",
+    },
+    created: function () {
+        this.getQuote();
+    },
+    methods: {
+        getQuote: function () {
+            const url = "https://api.chucknorris.io/jokes/random";
+            //use the axios get method
+            axios.get(url)
+                .then(response => {
+                    console.log(response.data.value);
+                    this.quote = response.data.value;
+                });
+        },
+    },
+});
